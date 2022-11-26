43acba65b731d59b5908a5f47f74250e599ac235
Abis <abisali@hotmail.com>
2022-11-26 18:58:59 +0400

    update

--- client/src/pages/AboutUsPage.js
+++ client/src/pages/AboutUsPage.js
@@ -5,7 +5,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 
 //Bootstrap-Bundle-JS-CSS
 import "bootstrap/dist/js/bootstrap.bundle.min";
@@ -16,20 +16,32 @@ import "../styles/aboutus.css";
 
 let About = () => {
   document.body.style.background = "#28C76F";
-    return (
-      <div className="aboutus-box">
-        <h1 className="main-heading"> &#10067; About Us  &#10067;</h1>
-        <div className="information-box">
-          <p className="information-para">Home cooks are our heroes—it's as simple as that. Allrecipes is a community built by and for kitchen experts: The cooks who will dedicate the weekend to a perfect beef bourguignon but love the simplicity of a slow-cooker rendition, too. The bakers who labor over a showstopping 9-layer cake but will just as happily doctor boxed brownies for a decadent weeknight dessert. The entertainers who just want a solid snack spread, without tons of dirty dishes at the end of the night.
+  return (
+    <div className="aboutus-box">
+      <h1 className="main-heading"> &#10067; About Us &#10067;</h1>
+      <div className="information-box">
+        <p className="information-para">
+          Home cooks are our heroes—it's as simple as that. Recipely is a
+          community built by and for kitchen experts: The cooks who will
+          dedicate the weekend to a perfect beef bourguignon but love the
+          simplicity of a slow-cooker rendition, too. The bakers who labor over
+          a showstopping 9-layer cake but will just as happily doctor boxed
+          brownies for a decadent weeknight dessert. The entertainers who just
+          want a solid snack spread, without tons of dirty dishes at the end of
+          the night.
+        </p>
 
-          </p>
-
-          <p className="information-para">Most importantly, Allrecipes connects home cooks with their greatest sources of inspiration — other home cooks. We're the world's leading digital food brand, and that inspires us to do everything possible to keep our community connected. Sixty-million home cooks deserve no less.</p>
-        </div>
+        <p className="information-para">
+          Most importantly, Recipely connects home cooks with their greatest
+          sources of inspiration — other home cooks. We're the world's leading
+          digital food brand, and that inspires us to do everything possible to
+          keep our community connected. Sixty-million home cooks deserve no
+          less.
+        </p>
       </div>
+      <Button className="back-button" variant="dark" href="/">Go Back</Button>
+    </div>
+  );
+};
 
-    
-    );
-  };
-  
-  export default About;
+export default About;
---
+++ client/src/pages/CreatePage.js
@@ -0,0 +1,11 @@
+//Base React libs
+import React from "react";
+
+let Create = () => {
+    return (
+        <div className="aboutus-box">
+            <h1 className="main-heading"> &#10067; Create a Recipe &#10067;</h1>
+        </div>
+    )
+}
+
