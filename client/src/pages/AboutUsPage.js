899db1bbe14d72a66b9386a9d1452f301c677881
Abis <abisali@hotmail.com>
2022-11-26 18:45:57 +0400

    updates

--- client/src/pages/AboutUsPage.js
+++ client/src/pages/AboutUsPage.js
@@ -17,22 +17,17 @@ import "../styles/aboutus.css";
 let About = () => {
   document.body.style.background = "#28C76F";
     return (
-      <Container fluid className="aboutus-main-container">
-        <Row className="row-one">
-          <Col className="col-one">
-          <h1>This is col 1</h1>
-          <div classname="box-left">
+      <div className="aboutus-box">
+        <h1 className="main-heading"> &#10067; About Us  &#10067;</h1>
+        <div className="information-box">
+          <p className="information-para">Home cooks are our heroes—it's as simple as that. Allrecipes is a community built by and for kitchen experts: The cooks who will dedicate the weekend to a perfect beef bourguignon but love the simplicity of a slow-cooker rendition, too. The bakers who labor over a showstopping 9-layer cake but will just as happily doctor boxed brownies for a decadent weeknight dessert. The entertainers who just want a solid snack spread, without tons of dirty dishes at the end of the night.
 
-          </div>
-          </Col>
-          <Col className="col-two">
-          <h1>This is col 2</h1>
-          </Col>
-        </Row>
-        <Row className="row-two"></Row>
-        <Row className="row-three"></Row>
+          </p>
 
-      </Container>
+          <p className="information-para">Most importantly, Allrecipes connects home cooks with their greatest sources of inspiration — other home cooks. We're the world's leading digital food brand, and that inspires us to do everything possible to keep our community connected. Sixty-million home cooks deserve no less.</p>
+        </div>
+      </div>
+
     
     );
   };
