b5e7fb62afdb6c486b06e0f026738be3f62fab06
Abis <abisali@hotmail.com>
2022-11-27 12:09:41 +0400

    Update Form.js

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -4,12 +4,9 @@ import React from "react";
 //Bootstrap-npm-lib
 import Button from "react-bootstrap/Button";
 
-
 //Bootstrap-Bundle-JS-CSS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-
 
 
 function RecipeForm() {
