434c478695d070094783615bf669a21e8762a1d3
Abis <abisali@hotmail.com>
2022-11-27 13:07:25 +0400

    update

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,5 +1,5 @@
 //Base React libs
-import React from "react";
+import React, { useState } from "react";
 
 //Bootstrap-npm-lib
 import Button from 'react-bootstrap/Button';
@@ -11,8 +11,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 
 function RecipeForm() {
+    const [email,setEmail] = useState('');
+
   return (
-    <h1>test</h1>
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Recipe</Form.Label>
+        <Form.Control type="text" placeholder="Enter title" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+      <p>{email}</p>
+    </Form>
+   
   );
 }
 
