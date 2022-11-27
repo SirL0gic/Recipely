33c3b7994bd9b1ec453f74dec6ffa1a1f3e28ad5
Abis <abisali@hotmail.com>
2022-11-27 13:17:26 +0400

    form update

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -4,24 +4,32 @@ import React, { useState } from "react";
 //Bootstrap-npm-lib
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 //Bootstrap-Bundle-JS-CSS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+//Custom CSS stylesheet
+import "../styles/create.css";
 
 
 function RecipeForm() {
     const [email,setEmail] = useState('');
 
   return (
-    <Form>
+    <Form className="form-container">
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Recipe</Form.Label>
         <Form.Control type="text" placeholder="Enter title" value={email} onChange={(e) => setEmail(e.target.value)} />
         <Form.Label>Author</Form.Label>
         <Form.Control type="text" placeholder="Enter author name" />
-        <Form.Label>Ingridents</Form.Label>
-        <Form.Control type="text" placeholder="Enter author name" />
+        <Form.Label>Ingredients</Form.Label>
+        <Form.Control type="textarea" placeholder="Enter Ingredients" />
+        <InputGroup>
+        <InputGroup.Text>With textarea</InputGroup.Text>
+        <Form.Control as="textarea" aria-label="With textarea" />
+      </InputGroup>
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
