18d2dd55ecebeb29934edc2d2ea039ac5e4ee07c
Abis <abisali@hotmail.com>
2022-11-27 14:40:25 +0400

    Update Form.js

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -2,9 +2,9 @@
 import React, { useState } from "react";
 
 //Bootstrap-npm-lib
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 
 //Bootstrap-Bundle-JS-CSS
 import "bootstrap/dist/js/bootstrap.bundle.min";
@@ -13,30 +13,37 @@ import "bootstrap/dist/css/bootstrap.min.css";
 //Custom CSS stylesheet
 import "../styles/create.css";
 
-
 function RecipeForm() {
-    const [email,setEmail] = useState('');
+  const [email, setEmail] = useState("");
 
   return (
     <Form className="form-container">
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Recipe</Form.Label>
-        <Form.Control type="text" placeholder="Enter title" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <Form.Control
+          type="text"
+          placeholder="Enter title"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br></br>
         <Form.Label>Author</Form.Label>
         <Form.Control type="text" placeholder="Enter author name" />
-        <Form.Label>Ingredients</Form.Label>
-        <Form.Control type="textarea" placeholder="Enter Ingredients" />
+        
+        <Form.Label></Form.Label>
         <InputGroup>
-        <InputGroup.Text>With textarea</InputGroup.Text>
-        <Form.Control as="textarea" aria-label="With textarea" />
-      </InputGroup>
+          <InputGroup.Text>Ingredients</InputGroup.Text>
+          <Form.Control as="textarea" aria-label="With textarea" />
+        </InputGroup>
+        <br></br>
+        <Form.Label>Small file input example</Form.Label>
+        <Form.Control type="file" size="sm" />
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
       </Button>
       <p>{email}</p>
     </Form>
-   
   );
 }
 
