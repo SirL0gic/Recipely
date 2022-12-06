de5aafcb4081f0b931914276e28c69ba64779f58
Abis <63680552+SirL0gic@users.noreply.github.com>
2022-12-06 15:21:16 +0400

    Create RecipeBook.js

---
+++ client/src/pages/RecipeBook.js
@@ -0,0 +1,13 @@
+//Base React libs
+import React from "react";
+
+//Bootstrap-npm-lib
+import Button from "react-bootstrap/Button";
+
+//Bootstrap-Bundle-JS-CSS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+//Custom CSS stylesheet
+import "../styles/aboutus.css";
+
