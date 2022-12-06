3944430e727cfa68c4cf2e5c6ce9905c00e884b7
Abis <63680552+SirL0gic@users.noreply.github.com>
2022-12-06 15:45:12 +0400

    Update RecipeBook.js

--- client/src/pages/RecipeBook.js
+++ client/src/pages/RecipeBook.js
@@ -2,7 +2,9 @@
 import React from "react";
 
 //Bootstrap-npm-lib
-import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row"
+import Container from "react-bootstrap/Container"
+import Col from "react-bootstrap/Col";
 
 //Bootstrap-Bundle-JS-CSS
 import "bootstrap/dist/js/bootstrap.bundle.min";
@@ -12,7 +14,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/aboutus.css";
 
 let Book = () => {
-    
+    return(
+        <Container className="main-container-book">
+            <Row className="row-one">
+                <Col className="col-one">
+                    
+                </Col>
+            </Row>
+            <Row></Row>
+        </Container>
+    )
     
 };
 
