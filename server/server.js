f7e26458dbcea388a5498390b1f3ede386a37256
Abis <abisali@hotmail.com>
2022-12-18 13:49:57 +0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,6 +5,15 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+app.post('/test', (req, res) => {
+  const data = req.body; //the request coming in from the front end
+  console.log(data);  // Log the data to the console
+
+  // Do something with the data here, such as saving it to a database
+
+  res.send({ message: 'Data received' });  // Send a response back to the frontend
+});
+
 app.listen(8000, () => {
   console.log('Server is listening on port 8000');
 });
