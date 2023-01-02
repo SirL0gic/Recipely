2a92163ac51f26715b7471d284232c86168f92f8
Abis <63680552+SirL0gic@users.noreply.github.com>
2023-01-02 16:57:43 +0400

    Update Search.jsx

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,14 +1,10 @@
-
-
-
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 //Base React libs
 import React, { useState } from "react";
 
 //Bootstrap-npm-lib
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-
 
 //Bootstrap-Bundle-JS-CSS
 import "bootstrap/dist/js/bootstrap.bundle.min";
@@ -23,32 +19,32 @@ let SearchBar = () => {
   // e is the event object
 
   const [title, setTitle] = useState("");
- 
+
   let handleSubmit = (eventobject) => {
     eventobject.preventDefault();
     console.log("Data submitted");
-    
-    // const recipeData = { 
+
+    // const recipeData = {
     // recipe_name: title,
     // };
 
     //function to remove current items from the page
-    function removeElementsByClass(className){
-        const elements = document.getElementsByClassName(className);
-        while(elements.length > 0){
-            elements[0].parentNode.removeChild(elements[0]);
-        }
-    };
+    function removeElementsByClass(className) {
+      const elements = document.getElementsByClassName(className);
+      while (elements.length > 0) {
+        elements[0].parentNode.removeChild(elements[0]);
+      }
+    }
 
     removeElementsByClass("cols");
 
-
     //The component that matches the search query is rendered.
-    ReactDOM.render(<RecipeReviewCard />, document.getElementById('recipe-cardslist'));
-   
+    ReactDOM.render(
+      <RecipeReviewCard />,
+      document.getElementById("recipe-cardslist")
+    );
+
     // window.location.replace("http://www.w3schools.com");
-
-
   };
 
   return (
